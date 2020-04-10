d773fc61e71752051e25b543e6dbf21d13f4f4b0
Egecan Citim <egecancitim@gmail.com>
2020-04-10 12:29:40 +0300

    add dotenv and fixed controllers, views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cocktails = open(url).read
+cocktail_result = JSON.parse(cocktails)
+cocktail_result.each
+cocktail_result['drinks'].each do |ingredient|
+  i = Ingredient.new(name: ingredient['strIngredient1'])
+  i.save
+end
+puts 'seeds completed'
